b837e5e6ea78642d935f603df092cbae611efca9
Eddie Nava <eddie.nava@gmail.com>
2022-09-15 12:21:30 -0400

    Modificaciones al Database

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 // Create a connection to database
 const db = new Sequelize({
   dialect: 'postgres',
-  host: 'dpg-cchbq85a4995s2ojckl0-a.oregon-postgres.render.com',
-  username: 'clonairbnb_user',
-  password: 'lZyWvBGXmP1NNXIDvz8vl1IdbxoeQFDh',
-  database: 'clonairbnb',
+  host: dpg-cchbq85a4995s2ojckl0-a.oregon-postgres.render.com,
+  username: clonairbnb_user,
+  password: lZyWvBGXmP1NNXIDvz8vl1IdbxoeQFDh,
+  database: clonairbnb
 
 });
 
